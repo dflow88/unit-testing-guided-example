acb2d7cb0d12c9bade3b156308c91c7b61f5a41d
Diego Flores <dieflope@gmail.com>
2021-06-22 10:41:57 -0600

    unit test sin terminar

--- calculator.js
+++ calculator.js
@@ -1,18 +1,34 @@
+console.log("Hola")
+
 function sum(a, b) {
-  return;
+  if (a === undefined && b === undefined){
+    return 0
+  } else if (a === undefined || b === undefined) {
+    return a + 0
+  }
+
+  return a + b;
 }
 
 function subtract(a, b) {
-  return;
+  if(a === undefined || b === undefined){
+  return a - b
+  }
 }
 
 function divide(a, b) {
-  return;
+  if (b === 0){
+    error
+  } else {
+  return a/b
+  }
 }
 
 function multiply(a, b) {
   return a * b;
 }
+
+console.log(sum(13,15))
 
 // The following is required to make unit tests work. Please ignore it.
 module.exports = { sum, subtract, divide, multiply };
